53b4715240b51f2fb4f283ff02033f3805df170d
Luiz Calaça <luizcalaca@Luizs-MacBook-Air.local>
2022-02-08 21:53:13 -0300

    NodeJS JWT Authentication

---
+++ users/user.service.js
@@ -0,0 +1,31 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+
+const users = [{ id: 1, username: 'name', password: 'pass', firstName: 'Name', lastName: 'Last' }];
+
+module.exports = {
+    authenticate,
+    getAll,
+};
+
+async function authenticate({ username, password }) {
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if (!user) throw 'Username or password is incorrect';
+
+    const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '7d' });
+
+    return {
+        ...omitPassword(user),
+        token
+    };
+}
+
+async function getAll() {
+    return users.map(user => omitPassword(user));
+}
+
+function omitPassword(user) {
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+}
